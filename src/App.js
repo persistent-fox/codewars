c6e8e85386499b70d0d0959ff769ae33f2b989c8
Margaret <rixten.6@gmail.com>
2024-06-17 20:28:05 +0300

    solve task 7kyu

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	function myFind(callback) {
-		for (let i = 0; i < this.length; i++) {
-			if (callback(this[i])) return this[i];
+	function arithmetic(a, b, operator) {
+		switch (operator) {
+			case 'add':
+				return a + b;
+			case 'subtract':
+				return a - b;
+			case 'multiply':
+				return a * b;
+			default:
+				return a / b;
 		}
 	}
 
-	Array.prototype.myFind = myFind;
-	const arr = [1, 2, 3, 2, 4, 5];
-	const arr2 = [
-		{ id: 1, name: 'Jason' },
-		{ id: 2, name: 'Tom Riddle' },
-		{ id: 3, name: 'Rita Bright' },
-	];
-	console.log(arr2.myFind(item => item.id === 2));
+	console.log(arithmetic(5, 6, 'add'));
 
 	return (
 		<div className='App'>
