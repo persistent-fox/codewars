9117f9f9ab1b55ccd714d6daae12f3540c487918
Margaret <rixten.6@gmail.com>
2024-07-12 17:14:42 +0300

    solve 7kyu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	function maxMultiple(divisor, bound) {
-		return bound - (bound % divisor);
+	function minValue(val) {
+		const arr = Array.from(new Set(val)).sort((a, b) => a - b);
+		return +arr.join('');
 	}
-
-	console.log(maxMultiple(2, 7));
+	console.log(minValue([5, 7, 5, 9, 7]));
+	console.log(minValue([1, 3, 1]));
+	console.log(minValue([1, 9, 3, 1, 7, 4, 6, 6, 7]));
 
 	return (
 		<div className='App'>
